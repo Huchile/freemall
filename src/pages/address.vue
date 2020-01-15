5682476f606430f7eab367755fbeecd0e08fe561
blue_angel <940054818@qq.com>
2020-01-15 15:31:11 +0800

    地址选配功能实现

--- src/pages/address.vue
+++ src/pages/address.vue
@@ -66,24 +66,24 @@
             <div class="addr-list-wrap">
               <div class="addr-list">
                 <ul>
-                  <li  class="check">
+                  <li v-for="(item,index) in addressFilter" :key="item.addressId" :class="{'check':checkedIndex == index}" @click="checkedIndex = index">
                     <dl>
-                      <dt>河畔一角</dt>
-                      <dd class="address">北京市昌平区</dd>
-                      <dd class="tel">17600000000</dd>
+                      <dt>{{item.userName}}</dt>
+                      <dd class="address">{{item.streetName}}</dd>
+                      <dd class="tel">{{item.tel}}</dd>
                     </dl>
                     <div class="addr-opration addr-del">
                       <!-- 删除地址 -->
-                      <a href="javascript:;" class="addr-del-btn">
+                      <a href="javascript:;" class="addr-del-btn" @click="delAddressComfirm(item.addressId)">
                         <svg class="icon icon-del">
                           <use xlink:href="#icon-del"></use>
                         </svg>
                       </a>
                     </div>
-                    <div class="addr-opration addr-set-default">
-                      <a href="javascript:;" class="addr-set-default-btn"><i>设为默认</i></a>
+                    <div class="addr-opration addr-set-default" v-if="!item.isDefault">
+                      <a href="javascript:;" class="addr-set-default-btn" @click="setDefault(item.addressId)"><i>设为默认</i></a>
                     </div>
-                    <div class="addr-opration addr-default">默认地址</div>
+                    <div class="addr-opration addr-default" v-if="item.isDefault">默认地址</div>
                   </li>
 
                   <li class="addr-new">
@@ -100,7 +100,7 @@
               </div>
 
               <div class="shipping-addr-more">
-                <a class="addr-more-btn up-down-btn open" href="javascript:;">
+                <a class="addr-more-btn up-down-btn" href="javascript:;" @click="expand" :class="{'open':limit>3}">
                   查看更多
                   <i class="i-up-down">
                     <i class="i-up-down-l"></i>
@@ -128,12 +128,20 @@
               </div>
             </div>
             <div class="next-btn-wrap">
-              <a class="btn btn--m btn--red" href="#">下一步</a>
+              <a class="btn btn--m btn--red" href="javascipt:;" @click="next">下一步</a>
             </div>
           </div>
         </div>
       </div>
       <nav-footer></nav-footer>
+      <modal :mdShow="modalConfirm" @close="modalConfirm = false">
+          <template v-slot:message>
+              <p>更多实战，请前往慕课学习新课《Vue全家桶从0打造小米商城》？</p>
+          </template>
+          <template v-slot:btnGroup>
+              <a  class="btn btn--m btn--red" href="javascript:;" @click="modalConfirm = false">关闭</a>
+          </template>
+      </modal>
   </div>
 </template>
 
@@ -147,10 +155,69 @@ export default {
   props: {
 
   },
+  data(){
+    return{
+        limit:3,
+        checkedIndex:0, //选中
+        modalConfirm:false,
+        addressList:[]
+    }
+  },
   components:{
       NavHeader,
       NavFooter,
       Modal
+  },
+  computed:{
+      addressFilter(){
+          return this.addressList.slice(0,this.limit)
+      }
+  },
+  mounted(){
+      this.init()
+  },
+  methods:{
+      //初始化
+      init(){
+          this.axios.get('/mock/address.json').then((response) => {
+              let res = response.data
+              this.addressList = res.data
+              res.data.forEach((item,index) => {
+                  if(item.isDefault){
+                      this.checkedIndex = index
+                  }
+              })
+          })
+      },
+      //点击查看更多
+      expand(){
+          if(this.limit == 3){
+              this.limit = this.addressList.length
+          }else{
+              this.limit = 3
+          }
+      },
+      //设为默认地址
+      setDefault(addressId){
+          this.addressList.map((item) => {
+              if(addressId == item.addressId){
+                  item.isDefault = true
+              }else{
+                  item.isDefault = false
+              }
+          })
+      },
+      //删除地址
+      delAddressComfirm(addressId){
+          this.addressList.map((item,index) => {
+              if(addressId == item.addressId){
+                 this.addressList.splice(index,1)
+              }
+          })
+      },
+      next(){
+          this.modalConfirm = true
+      }
   }
 }
 </script>
